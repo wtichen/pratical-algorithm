610d9aa052936d26ba05459bf73d591da80ff64e
Chen Wei Ting <wtichen.tw@gmail.com>
2016-12-04 21:14:39 +0800

    :poop: Complete milestone 1 plausibly

--- hw1-SAT_sudoku_solver/main.cpp
+++ hw1-SAT_sudoku_solver/main.cpp
@@ -4,45 +4,152 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <unistd.h>
+#include <cstdlib>
 
 using namespace std;
 
 void print();
 void output_sat();
-void output_puz();
-void make_cls(string opt, int idx, int num);
-int get_num_pos(string opt, int idx, int num);
+void output_puz(string res);
+void parse_line();
+void parse_file();
+void parse_result();
+void make_cell_cls(int r, int c);
+void make_bloc_cls(int idx, int num);
+void make_rc_cls(int idx, int num);
 int get_rcn_encode(int r, int c, int n);
 int* get_rcn_decode(int code);
+bool is_solvable();
 
-int N = 0, n = 0;
+int N = 0;                      // Sudoku edge size
+int n = 0;                      // Sudoku block size
 int cls_idx = 0;
-int G[16][16];
-string lines[16];
-string cls[3000000] = { "" };
+int G[100][100];                  // Unsolved Sudoku grid
+int _G[100][100];                 // Solved Sudoku grid
+string lines[100];
+string cls[1000000] = { "" };
 stringstream ss;
 fstream unsol_puz, sol_puz;
 fstream unsol_sat, sol_sat;
 
-string unsol_sat_filename = "unsol_sat";
-string sol_sat_filename = "sol_sat";
 
 int main(int argc, char* argv[])
 {
-    // argc handle
 
-    // Initialization
+    string cmd;
+    string unsol_sat_filename = "unsol_sat";
+    string sol_sat_filename = "unsol_sat.sat";
+    string unsol_puz_filename = string(argv[1]);
+    string sol_puz_filename = string(argv[2]);
+    string minisat_bin = string(argv[3]);
 
     // Open files
-    unsol_puz.open(argv[1], ios::in);
+    unsol_puz.open(unsol_puz_filename, ios::in);
     unsol_sat.open(unsol_sat_filename, ios::out);
-    
-    // Calculate length
+
+    // Parsing
+    parse_line();
+    parse_file();
+
+    // For each cell
+    for (int r = 0; r < N; r++) {
+        for (int c = 0; c < N; c++) {
+            make_cell_cls(r, c);
+        }
+    }
+
+    // For each row, col, block 
+    for (int idx = 0; idx < N; idx++) {
+        for (int num = 0; num < N; num++) {
+            make_rc_cls(idx, num);
+            make_bloc_cls(idx, num);
+        }
+    }
+
+    // Output and close unsol SAT and puz file 
+    output_sat();
+
+    // Close files
+    unsol_sat.close();
+    unsol_puz.close();
+
+    // Call minisat solver
+    cmd = "./" + minisat_bin + " " + unsol_sat_filename + " " + sol_sat_filename;
+    system(cmd.c_str());
+
+    // Open solved SAT file and output puzzle file
+    sol_sat.open(sol_sat_filename, ios::in);
+    sol_puz.open(sol_puz_filename, ios::out);
+
+    // Decode from CNF 
+    is_solvable() ? output_puz("SUCCESS") : output_puz("FAIL");
+
+    // Close sol SAT and puz file
+    sol_sat.close();
+    sol_puz.close();    
+
+    /* // Clean up */
+    /* cmd = "rm " + unsol_sat_filename + " " + sol_sat_filename; */
+    /* system(cmd.c_str()); */
+
+    return 0;
+}
+
+int get_rcn_encode(int r, int c, int n)
+{
+    return r*N*N + c*N + n + 1;  // Shift from 0~728 to 1~729
+}
+
+int* get_rcn_decode(int code)
+{
+    int* ans = new int[3];
+
+    code -= 1;              // Shift from 1~729 to 0~728
+
+    ans[2] = code%N + 1;    // Shift from 0~8 to 1~9
+    code /= N;
+    ans[1] = code%N;
+    code /= N;
+    ans[0] = code%N;
+
+    return ans;
+}
+
+bool is_solvable()
+{
+    string res;
+    ss << sol_sat.rdbuf();
+    ss >> res;
+
+    if (res == "SAT") {
+        int val = 0;
+
+        // Parse result
+        while (ss >> val) {
+            if (val > 0) {
+                int* ans = get_rcn_decode(val);
+                int r = ans[0], c = ans[1], n = ans[2];
+                _G[r][c] = n;
+            }
+        }
+
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+}
+
+void parse_line()
+{
     while (getline(unsol_puz, lines[N])) N++;
     n = sqrt(N);
-    
-    // Parse lines to G matrix
+}
+
+void parse_file()
+{
     for (int i = 0; i < N; ++i) {
         ss << lines[i];
         for (int j = 0; j < N; ++j) {
@@ -51,233 +158,6 @@ int main(int argc, char* argv[])
         ss.str("");
         ss.clear();
     }
-
-    // Prefill
-    for (int r = 0; r < N; r++) {
-        for (int c = 0; c < N; c++) {
-            if (G[r][c] != 0) {
-                cls[cls_idx++] = to_string(r) + to_string(c) + to_string(G[r][c]) + " 0";
-            }
-        }
-    }
-
-    // Generate row cls
-    for (int r = 0; r < N; r++) {
-        for (int num = 1; num <= N; num++) {
-            int num_pos = get_num_pos("ROW", r, num);
-
-            if (num_pos == -1) {
-                make_cls("ROW", r, num);
-                 
-            }
-        }
-    }
-
-    // Generate col cls
-    for (int c = 0; c < N; c++) {
-        for (int num = 1; num <= N; num++) {
-            int num_pos = get_num_pos("COL", c, num);
-
-            if (num_pos == -1) {
-                make_cls("COL", c, num);
-
-            } 
-        }
-    }
-
-    // Generate bloc cls
-    for (int b = 0; b < N; b++) {
-        for (int num = 1; num <= N; num++) {
-            int num_pos = get_num_pos("BLOC", b, num);
-
-            if (num_pos == -1) {
-                make_cls("BLOC", b, num);
-            } 
-        }
-    }
-
-    // One cell for one num
-    for (int r = 0; r < N; r++) {
-        for (int c = 0; c < N; c++) {
-
-            if (G[r][c] == 0) {
-                // At least one
-                // for (int num = 1; num <= N; num++) {
-                    // cls[cls_idx] += to_string(r+1) + to_string(c+1) + to_string(num) + " ";
-                // }
-
-                // cls[cls_idx++] += "0";
-
-                // At most one
-                for (int i = 1; i <= N; i++) {
-                    for (int j = i+1; j <= N; j++) {
-                        cls[cls_idx] += "-" + to_string(r) + to_string(c) + to_string(i) + " ";        
-                        cls[cls_idx] += "-" + to_string(r) + to_string(c) + to_string(j) + " ";        
-                        cls[cls_idx++] += "0";
-                    }
-                } 
-            }
-
-        }
-    }
-
-
-
-    output_sat();
-    unsol_sat.close();
-    unsol_puz.close();
-
-    // Call minisat solver
-    system("./minisat unsol_sat sol_sat");
-    // execlp("./minisat", unsol_sat_filename, sol_sat_filename, NULL);
-    // exec
-    
-
-    sol_sat.open(sol_sat_filename, ios::in);
-    sol_puz.open(argv[2], ios::out);
-
-    // Decode from CNF 
-    int val;
-    string res;
-
-    ss << sol_sat.rdbuf();
-    ss >> res;
-    if (res == "SAT") {
-        while (ss >> val) {
-            if (val > 0) {
-                cout << val << " ";
-            }
-        }
-    }
-
-    return 0;
-
-}
-
-void make_cls(string opt, int idx, int num)
-{
-    if (opt == "ROW") {
-        // At least one 
-        for (int i = 0; i < N; i++) {
-            if (G[idx][i] == 0) {
-                cls[cls_idx] += to_string(idx) + to_string(i) + to_string(num) + " "; 
-            }
-        } 
-
-        cls[cls_idx++] += "0";
-            
-        // At most one
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                if (G[idx][i] == 0 && G[idx][j] == 0) {
-                    cls[cls_idx] += "-" + to_string(idx) + to_string(i) + to_string(num) + " ";
-                    cls[cls_idx] += "-" + to_string(idx) + to_string(j) + to_string(num) + " ";
-                    cls[cls_idx++] += "0";
-                }
-           }
-        }
-    } 
-
-    if (opt == "COL") {
-        // At least one 
-        for (int i = 0; i < N; i++) {
-            if (G[i][idx] == 0) {
-                cls[cls_idx] += to_string(i) + to_string(idx) + to_string(num) + " "; 
-            }
-        } 
-
-        cls[cls_idx++] += "0";
-
-        // At most one
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
-                if (G[i][idx] == 0 && G[j][idx] == 0) {
-                    cls[cls_idx] += "-" + to_string(i) + to_string(idx) + to_string(num) + " ";
-                    cls[cls_idx] += "-" + to_string(j) + to_string(idx) + to_string(num) + " ";
-                    cls[cls_idx++] += "0";
-                }
-            }
-        }
-    } 
-
-    if (opt == "BLOC") {
-        int r_start = floor(idx/n) * n;
-        int c_start = (idx%n) * n;
-        int rc_ls_idx = 0;
-        string *rc_ls = new string[N*(N-1)/2];
-        
-        // At least one
-        for (int r = r_start; r < r_start + n; r++) {
-            for (int c = c_start; c < c_start + n; c++) {
-                if (G[r][c] == 0) {
-					cls[cls_idx] += to_string(r) + to_string(c) + to_string(num) + " ";
-					rc_ls[rc_ls_idx++] = to_string(r) + to_string(c);
-                }
-            } 
-        }
-
-        cls[cls_idx++] += " 0";
-
-        // At most one
-        for (int i = 0; i < rc_ls_idx; i++) {
-            for (int j = i+1; j < rc_ls_idx; j++) {
-                string p1 = rc_ls[i];
-                string p2 = rc_ls[j];
-
-
-                // if (G[r1][c1] == 0 && G[r2][c2] == 0) {
-                    cls[cls_idx] += "-" + p1 + to_string(num) + " "; 
-                    cls[cls_idx] += "-" + p2 + to_string(num) + " "; 
-                    cls[cls_idx++] += "0";
-                // }
-
-            } 
-        }
-
-    }
-}
-
-int get_rcn_code(int r, int c, int n)
-{
-    return r*N*N + c*N + n;
-}
-
-int get_num_pos(string opt, int idx, int num)
-{
-
-    // Search row
-    if (opt == "ROW") {
-        for (int i = 0; i < N; i++) {
-            if (G[idx][i] == num) {
-                return i;
-            }
-        }
-    }
-
-    // Search col
-    if (opt == "COL") {
-        for (int i = 0; i < N; i++) {
-            if (G[i][idx] == num) {
-                return i;
-            }
-        }
-    }
-
-    if (opt == "BLOC") {
-        int r_start = floor(idx/n) * n;
-        int c_start = (idx%n) * n;
-
-        for (int i = r_start; i < r_start + n; i++) {
-            for (int j = c_start; j < c_start + n; j++) {
-                if (G[i][j] == num) {
-                    return 1;
-                }
-            } 
-        }
-
-    }
-
-    return -1;
 }
 
 void output_sat()
@@ -290,11 +170,123 @@ void output_sat()
 
 }
 
+void output_puz(string res)
+{
+    if (res == "SUCCESS") {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sol_puz << _G[i][j] << " "; 
+            }
+            sol_puz << endl;
+        }
+    }
+    else {
+        cout << "NO" << endl;
+        sol_puz << "NO ANSWER" << endl; 
+    }
+}
+
+void make_rc_cls(int idx, int num)
+{
+    /* ROW */
+    // At least one 
+    for (int i = 0; i < N; i++) {
+        cls[cls_idx] += to_string(get_rcn_encode(idx, i, num)) + " ";
+    } 
+    cls[cls_idx++] += "0";
+
+    // At most one
+    for (int i = 0; i < N; i++) {
+        for (int j = i+1; j < N; j++) {
+            cls[cls_idx] += "-" + to_string(get_rcn_encode(idx, i, num)) + " ";
+            cls[cls_idx] += "-" + to_string(get_rcn_encode(idx, j, num)) + " ";
+            cls[cls_idx++] += "0";
+        }
+    }
+
+    /* COL */
+    // At least one 
+    for (int i = 0; i < N; i++) {
+        cls[cls_idx] += to_string(get_rcn_encode(i, idx, num)) + " ";
+    } 
+    cls[cls_idx++] += "0";
+
+    // At most one
+    for (int i = 0; i < N; i++) {
+        for (int j = i+1; j < N; j++) {
+            cls[cls_idx] += "-" + to_string(get_rcn_encode(i, idx, num)) + " ";
+            cls[cls_idx] += "-" + to_string(get_rcn_encode(j, idx, num)) + " ";
+            cls[cls_idx++] += "0";
+        }
+    }
+
+}
+
+void make_bloc_cls(int idx, int num)
+{
+    
+    int r_start = floor(idx/n) * n;
+    int c_start = (idx%n) * n;
+
+    int ls_idx = 0;
+    int* r_ls = new int[N*(N-1)/2];
+    int* c_ls = new int[N*(N-1)/2];
+
+    // At least one
+    for (int r = r_start; r < r_start + n; r++) {
+        for (int c = c_start; c < c_start + n; c++) {
+            cls[cls_idx] += to_string(get_rcn_encode(r, c, num)) + " ";
+            r_ls[ls_idx] = r;
+            c_ls[ls_idx] = c;
+            ls_idx++;
+        } 
+    }
+    cls[cls_idx++] += " 0";
+
+    // At most one
+    for (int i = 0; i < ls_idx; i++) {
+        for (int j = i+1; j < ls_idx; j++) {
+            string p1 = to_string(get_rcn_encode(r_ls[i], c_ls[i], num));
+            string p2 = to_string(get_rcn_encode(r_ls[j], c_ls[j], num));
+
+            cls[cls_idx] += "-" + p1 + " "; 
+            cls[cls_idx] += "-" + p2 + " "; 
+            cls[cls_idx++] += "0";
+        } 
+    }
+
+}
+
+void make_cell_cls(int r, int c)
+{
+    // For each cell
+
+    if (G[r][c] != 0) {
+        cls[cls_idx++] = to_string(get_rcn_encode(r, c, G[r][c]-1)) + " 0";
+    }
+    else {
+        // At least one
+        for (int num = 0; num < N; num++) {
+            cls[cls_idx] += to_string(get_rcn_encode(r, c, num)) + " ";
+        }
+        cls[cls_idx++] += "0";
+
+        // At most one
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                cls[cls_idx] += "-" + to_string(get_rcn_encode(r, c, i)) + " ";        
+                cls[cls_idx] += "-" + to_string(get_rcn_encode(r, c, j)) + " ";        
+                cls[cls_idx++] += "0";
+            }
+        } 
+    }
+}
+
 void print()
 {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cout << G[i][j] << " "; 
+            cout << _G[i][j] << " "; 
         }
         cout << endl;
     }
---
+++ proj-SAT_solver/sat.cpp
@@ -0,0 +1,308 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include "parser.h"
+
+using namespace std;
+
+bool DPLL();
+void make_implication(int* &XI, int &XI_idx);
+bool check_success();
+bool check_fail();
+void init();
+void update(int x);
+int get_MOM_literal();
+bool is_X_in_cl(int idx, int val);
+int abs(int n);
+
+/* Const Variable */
+static const int MOM_THRESHHOLD = 3;
+
+/* Global Variable */
+vector<vector<int> > cls; 
+fstream sol;
+int cls_size;
+int X_max;
+int* X;
+int* cl_val;
+int* cl_len;
+int* cl_omega;
+
+int main(int argc, char **argv)
+{
+    string in_fn  = argv[1];
+    string out_fn = in_fn.substr(0, in_fn.find(".")) + ".sat";
+    /* string out_fn = in_fn.substr(in_fn.find_last_of("/")+1, in_fn.find(".") - in_fn.find_last_of("/")) + "sat"; */
+    
+    //parse_DIMACS_CNF(cls, X_max, in_fn.c_str());
+    parse_DIMACS_CNF(cls, X_max, "./benchmarks/SAT/sanity/sanity2.cnf");
+    cls_size = cls.size();
+
+
+    X = new int[X_max+1];
+    cl_val = new int[cls_size+1];
+    cl_len = new int[cls_size+1];
+    cl_omega = new int[cls_size+1];
+
+    init();
+
+    sol.open(out_fn, ios::out);
+    if (DPLL()) {
+        sol << "s SATISFIABLE" << endl;
+        sol << "v ";
+
+        for (int i = 1; i < X_max+1; i++) {
+            if (X[i] == 0) sol << "-";
+            sol << i << " ";
+        }
+        sol << " 0";
+
+    } else {
+        sol << "s UNSATISFIABLE" << endl;
+    }
+    sol.close();
+
+    return 0;
+}
+
+
+void output_X()
+{
+    for (int i = 1; i < X_max+1; i++) {
+        cout << "x" << i << ": " << X[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < cls_size; i++) {
+        cout << setw(5) << "cl" << i << " "; 
+
+        for (int j = 0; j < cl_len[i]; j++) {
+            cout << setw(3) << cls[i][j] << " " ;
+        }
+
+        cout << " val: " << cl_val[i] << endl;
+    }
+}
+
+bool DPLL()
+{
+
+    /* output_X(); */
+
+    int XI_idx_old = -1;
+    int XI_idx = 0;
+    int* XI = new int[X_max];
+
+    while(XI_idx != XI_idx_old) {
+        /* cout << "Old: " << XI_idx_old << endl; */
+        /* cout << "New: " << XI_idx << endl; */
+        XI_idx_old = XI_idx;
+        make_implication(XI, XI_idx);
+    }
+
+
+    if (check_success()) 
+        return true;
+
+    if (check_fail()) {
+        /* cout << "## FAIl FAIL FAIL" << endl; */
+
+        /* cout << "## RESTORING "; */
+        for (int i = 1; i < XI[0]; i++) {
+            /* cout << XI[i] << " "; */
+            X[XI[i]] = -1;
+
+
+        }
+        /* cout << endl; */
+
+        return false;
+    }
+
+
+
+    int x = get_MOM_literal();
+    X[x] = 0;
+    /* cout << "## ASSIGN x" << x << " AS 0" << endl; */
+    update(x*-1);
+
+    if (DPLL())
+        return true;
+    else {
+        X[x] = 1;
+        /* cout << "## ASSIGN x" << x << " AS 1" << endl; */
+        update(x*1);
+        return DPLL();
+    }
+
+}
+
+void init()
+{
+    for (int i = 0; i < cls_size+1; i++) {
+        cl_val[i] = -1;
+        cl_len[i] = cls[i].size();
+        cl_omega[i] = cls[i].size();
+    }
+
+
+    for (int i = 0; i < X_max+1; i++) {
+        X[i] = -1;
+    }
+}
+
+void update(int xx)
+{
+    /* for (int i = 0; i < cls_size; i++) { */
+
+    /*     // Update cl containing x */
+    /*     if (is_X_in_cl(i, x)) { */
+    /*         cl_omega[i]--; */
+    /*         cl_val[i] = 1; */
+    /*     } */
+
+    /*     // Update cl containing -x */
+    /*     if (is_X_in_cl(i, x*-1)) { */
+    /*         cl_omega[i]--; */
+    /*         if (cl_omega[i] == 0 && cl_val[i] != 1) { */
+    /*             cl_val[i] = 0; */
+    /*         } */
+    /*     } */
+
+    /* } */
+
+    for (int i = 0; i < cls_size; i++) {
+        for (int j = 0; j < cl_len[i]; j++) {
+            int x = cls[i][j];
+            int idx = abs(x);
+
+            if ((X[idx] == 0 && x < 0) || (X[idx] == 1 && x > 0)) {
+                cl_val[i] = 1;
+                break;
+            }
+            else if (X[idx] == -1) {
+                cl_val[i] = -1;
+                break;
+            }
+            else {
+                cl_val[i] = 0;
+            }
+        }
+    }
+}
+
+int get_MOM_literal()
+{
+    int* cnt = new int[cls_size+1];
+
+    // Init
+    for (int i = 0; i < cls_size+1; i++) {
+        cnt[i] = 0;
+    }
+
+    // Count for each X
+    for (int i = 0; i < cls_size; i++) {
+
+        // Check threshhold
+        if (cl_omega[i] <= MOM_THRESHHOLD) {
+            for (int j = 0; j < cl_len[i]; j++) {
+                int x = abs(cls[i][j]);
+                /* cout << x << " "; */
+
+                // Check for +x only and check unassigned
+                if (X[x] == -1) {
+                    cnt[x]++; 
+                }
+            } 
+        }
+
+        /* cout << endl; */
+    }
+
+    // Select most frequent one
+    int idx = 1, max = 0;
+    for (int i = 1; i < cls_size+1; i++) {
+        if (cnt[i] > max) {
+            max = cnt[i]; 
+            idx = i;
+        }
+    }
+
+    delete [] cnt;
+
+    return idx;
+}
+
+bool is_X_in_cl(int idx, int val)
+{
+    for (int i = 0; i < cl_len[idx]; i++) {
+        if (cls[idx][i] == val)
+            return true;
+    }
+
+    return false;
+}
+
+
+void make_implication(int* &XI, int &XI_idx)
+{
+
+    for (int i = 0; i < cls_size; i++) {
+        if (cl_omega[i] == 1 && cl_val[i] == 0) {
+
+            for (int j = 0; j < cl_len[i]; j++) {
+                int x = cls[i][j];
+
+                // If unassigned
+                if (X[abs(x)] == -1) {
+                    
+                    XI[XI_idx++] = abs(x);
+
+                    if (x < 0) {
+                        X[abs(x)] = 0;
+                        cout << "## IMPLY x" << abs(x) << " AS 0 AT CLS" << i << endl;
+                        update(abs(x)*-1);
+                    } 
+                    else {
+                        X[abs(x)] = 1;
+                        cout << "## IMPLY x" << abs(x) << " AS 1 AT CLA" << i << endl;
+                        update(abs(x)*1);
+                    }
+                }
+            }
+        }
+    }
+
+    XI[0] = XI_idx;
+}
+
+bool check_success() 
+{
+    for (int i = 0; i < cls_size; i++) {
+
+        // if have 0(false) -1(unassign) -> no success
+        if (cl_val[i] != 1) 
+            return false;
+    }
+
+    return true;
+}
+
+bool check_fail()
+{
+    for (int i = 0; i < cls_size; i++) {
+        
+        // Have 0, fail
+        if (cl_val[i] == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int abs(int n)
+{
+    return n > 0 ? n: -n;
+}
